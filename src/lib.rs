d3bcd66bac64f165a334535c273607dda7b0d9e2
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2025-07-11 13:41:22 -0700

    feat: new public function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 pub fn reverse(input: impl Into<String>) -> String {
     let s: String = input.into();
 
@@ -10,12 +12,26 @@ pub fn reverse2(input: impl Into<String>) -> String {
     s.chars().rev().collect::<String>()
 }
 
+#[must_use]
+pub fn to_bytes(input: Cow<'_, str>) -> Vec<u8> {
+    input.into_owned().into_bytes()
+}
+
 #[cfg(test)]
 mod tests {
     use crate::reverse;
+    use crate::to_bytes;
 
     #[test]
     fn it_works() {
         assert_eq!("fotsirk", reverse("kristof"));
+    }
+
+    #[test]
+    fn into_bytes() {
+        assert_eq!(
+            to_bytes("Hello, World".into()),
+            &[72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100]
+        );
     }
 }
